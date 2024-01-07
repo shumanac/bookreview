367b4419bfaa5c6008ed9b735a2353b5a1a400aa
shumana <shumana@karmaimpact.com>
2024-01-07 01:58:49 -0500

    index file changed

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,21 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+
+ const token = req.headers.authorization || req.cookies.token;
+
+
+ if (!token) {
+   return res.status(401).json({ error: 'Unauthorized - Token missing' });
+ }
+
+ try {
+   const decoded = jwt.verify(token, 'jlklkl800909');
+   req.user = decoded;
+   next();
+ } catch (err) {
+   return res.status(401).json({ error: 'Unauthorized - Invalid token' });
+ }
 });
  
 const PORT =5000;
