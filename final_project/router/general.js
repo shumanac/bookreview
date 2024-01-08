03677134e9a1b766eca6db25564dbb97cbffa80e
shumana <shumana@karmaimpact.com>
2024-01-08 13:00:41 -0500

    task2 task3 added

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,43 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  try {
+    res.json({ books });
+  } catch (error) {
+    console.error('Error getting books:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  try {
+    const requestedIsbn = req.params.isbn;
+    const book = books.find((b) => b.isbn === requestedIsbn);
+
+    if (!book) {
+      return res.status(404).json({ error: 'Book not found' });
+    }
+    res.json({ book });
+  } catch (error) {
+    console.error('Error getting book details:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  try {
+    const requestedAuthor = req.params.author;
+    const matchingBooks = books.filter((b) => b.author === requestedAuthor);
+    if (matchingBooks.length === 0) {
+      return res.status(404).json({ error: 'No books found for the author' });
+    }
+    res.json({ books: matchingBooks });
+  } catch (error) {
+    console.error('Error getting book details by author:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Get all books based on title
